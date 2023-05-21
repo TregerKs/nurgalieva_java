ff7804ae9e73521f928c6c1d4c39437958799efe
TregerKs <lim-atata@yandex.ru>
2023-05-22 01:06:09 +0300

    minor updates

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
                 System.out.println("Привет");
             }
         } else if (value.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
             System.out.println("Нет такого имени");
         }
     }
